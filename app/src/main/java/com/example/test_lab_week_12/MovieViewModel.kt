f876abfb4759cf1e75a7bdd52acd4582227c1761
Felicia Sabatini <feliciasabatinigunawan2005@gmail.com>
2025-11-27 17:30:30 +0700

    Commit No.3 – Assignment

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -1,35 +1,31 @@
 package com.example.test_lab_week_12
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.Dispatchers
 import com.example.test_lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
-
+import kotlinx.coroutines.launch
 
 class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
-    init{
+    init {
         fetchPopularMovies()
     }
 
-    private val _popularMovies = MutableStateFlow(
-            emptyList<Movie>()
-            )
+    private val _popularMovies = MutableStateFlow<List<Movie>>(emptyList())
     val popularMovies: StateFlow<List<Movie>> = _popularMovies
 
     private val _error = MutableStateFlow("")
     val error: StateFlow<String> = _error
 
-    private fun fetchPopularMovies(){
+    private fun fetchPopularMovies() {
         viewModelScope.launch(Dispatchers.IO) {
             movieRepository.fetchMovies().catch {
                 _error.value = "An exception occurred: ${it.message}"
-            }.collect{
-                _popularMovies.value = it
+            }.collect { movies ->
+                _popularMovies.value = movies.sortedByDescending { it.popularity }
             }
         }
     }
